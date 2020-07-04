d9bc89aeb69f4628bf966bf141c51e0cd10e9378
AlexAlz <daagblaze98@gmail.com>
2020-07-04 18:36:46 -0500

    Correcciones menores

--- conecta.php
+++ conecta.php
@@ -1,11 +1,29 @@
 <?php
-define ("HOST",'localhost');
+/*define ("HOST",'localhost:3306');
 define ("BD",'proyecto');
 define ("USER_BD",'root');
 define ("PASS_BD",'');
+define ("PORT", "3306");
 
 function conecta(){
-    if(!($con = mysqli_connect(HOST,USER_BD,PASS_BD)))   {
+    if(!($con = mysqli_connect(HOST,USER_BD,PASS_BD,BD,PORT)))   {
+        echo "Error conectando al Servidor de BD";
+        exit();
+    }
+    if(!mysqli_select_db($con, BD))   {
+        echo "Error seleccionando BD";
+        exit();
+    }
+    return $con;
+}*/
+define ("HOST","db4free.net");
+define ("BD","proyecto_final");
+define ("USER_BD","alejandro_alz");
+define ("PASS_BD","12345678");
+define ("PORT", "3306");
+
+function conecta(){
+    if(!($con = mysqli_connect(HOST,USER_BD,PASS_BD,BD,PORT)))   {
         echo "Error conectando al Servidor de BD";
         exit();
     }
